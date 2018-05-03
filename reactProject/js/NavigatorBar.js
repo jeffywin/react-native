f7b977f03192507badc45dc37ab112f9056ca8ed
jeffywin <jeffywin@126.com>
2018-05-03 22:45:17 +0800

    navigatorBar

---
+++ reactProject/js/NavigatorBar.js
@@ -0,0 +1,27 @@
+/**
+ * Created by jeffywin on 2018/5/3.
+ */
+
+import React, {PropTypes} from 'react'
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet
+} from 'react-native';
+
+const NAV_BAR_HEIGHT_ANDROID = 45;
+const NAV_BAR_HEIGHT_IOS = 44;
+const STATUS_BAR_HEIGHT = 20;
+
+class NavigatorBar extends React.Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default NavigatorBar
--- reactProject/js/girl.js
+++ reactProject/js/girl.js
@@ -11,26 +11,26 @@ import {
 
 export default class Girl extends React.Component {
     constructor(props) {
-        super(props)
+      super(props)
     }
     render() {
-        return(
-            <View style={styles.container}>
-                <Text>我是女孩</Text>
-                <Text>{this.props.word}</Text>
-                <Text onPress={() => {
-                    this.props.onBack('一盒巧克力')
-                    this.props.navigator.pop()
-                }}>回赠男孩</Text>
-            </View>
-        )
+      return(
+        <View style={styles.container}>
+          <Text>我是女孩</Text>
+          <Text>{this.props.word}</Text>
+          <Text onPress={() => {
+            this.props.onBack('一盒巧克力')
+            this.props.navigator.pop()
+          }}>回赠男孩</Text>
+        </View>
+      )
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#c33',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#c33',
+    justifyContent: 'center'
+  }
 })
