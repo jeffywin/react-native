2f78c4831132d066fbe1a76752d24a4321c66f53
jeffywin <jeffywin@126.com>
2018-05-06 22:25:52 +0800

    navi

--- reactProject/js/NavigatorBar.js
+++ reactProject/js/NavigatorBar.js
@@ -15,13 +15,40 @@ const NAV_BAR_HEIGHT_IOS = 44;
 const STATUS_BAR_HEIGHT = 20;
 
 class NavigatorBar extends React.Component {
-  render() {
-    return(
-      <div>
+  static propTypes = {
+    style: View.propTypes.style,
+    title: PropTypes.title,
+    titleView: PropTypes.titleView,
+    hidden: PropTypes.hidden,
+    leftButton: PropTypes.leftButton,
+    rightButton: PropTypes.rightButton
+  }
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      hide: false
+    }
+  }
 
-      </div>
+  render() {
+    let titleView = this.props.titleView ? this.props.titleView : <Text>{this.props.title}</Text>
+    let content = <View>
+      {this.props.leftButton}
+      {this.props.rightButton}
+    </View>
+    return(
+      <View style={styles.container}>
+
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.creat({
+  container: {
+    background: '#c33'
+  }
+})
 
 export default NavigatorBar
