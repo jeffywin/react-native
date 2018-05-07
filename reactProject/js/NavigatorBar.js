f507ef99441a151692d6af10863c42b9f364803f
jeffywin <jeffywin@126.com>
2018-05-07 23:07:59 +0800

    propsTypes

--- reactProject/js/NavigatorBar.js
+++ reactProject/js/NavigatorBar.js
@@ -2,29 +2,31 @@
  * Created by jeffywin on 2018/5/3.
  */
 
-import React, {PropTypes} from 'react'
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {
   View,
   Text,
   Image,
-  StyleSheet
+  StyleSheet,
+  Platform
 } from 'react-native';
 
 const NAV_BAR_HEIGHT_ANDROID = 45;
 const NAV_BAR_HEIGHT_IOS = 44;
 const STATUS_BAR_HEIGHT = 20;
 
-class NavigatorBar extends React.Component {
+export default class NavigationBar extends Component {
   static propTypes = {
     style: View.propTypes.style,
-    title: PropTypes.title,
-    titleView: PropTypes.titleView,
-    hidden: PropTypes.hidden,
-    leftButton: PropTypes.leftButton,
-    rightButton: PropTypes.rightButton
+    title: PropTypes.string,
+    titleView: PropTypes.element,
+    hidden: PropTypes.bool,
+    leftButton: PropTypes.element,
+    rightButton: PropTypes.element,
   }
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       title: '',
       hide: false
@@ -32,23 +34,46 @@ class NavigatorBar extends React.Component {
   }
 
   render() {
-    let titleView = this.props.titleView ? this.props.titleView : <Text>{this.props.title}</Text>
-    let content = <View>
+    let titleView = this.props.titleView ? this.props.titleView : <Text style={styles.title}>{this.props.title}</Text>
+    let content = <View style={styles.navBar}>
       {this.props.leftButton}
+      <View style={styles.titleViewContainer}>
+        {titleView}
+      </View>
       {this.props.rightButton}
     </View>
     return(
       <View style={styles.container}>
-
+        {content}
       </View>
     )
   }
 }
 
-const styles = StyleSheet.creat({
+const styles = StyleSheet.create({
   container: {
-    background: '#c33'
-  }
+    backgroundColor: 'gray',
+  },
+  navBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: Platform.OS === 'ios' ? NAV_BAR_HEIGHT_IOS : NAV_BAR_HEIGHT_ANDROID,
+  },
+  navBarTitleContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    left: 40,
+    top: 0,
+    right: 40,
+    bottom: 0,
+  },
+  title: {
+    fontSize: 20,
+    color: '#FFFFFF',
+  },
+  navBarButton: {
+    alignItems: 'center',
+  },
 })
-
-export default NavigatorBar
