3faf6e9364016ea9ce02013f6802fff12fe30cf7
jeffywin <jeffywin@126.com>
2018-07-04 21:38:14 +0800

    fetch

--- reactProject/js/expand/DataRespository.js
+++ reactProject/js/expand/DataRespository.js
@@ -1,3 +1,14 @@
-export default class DataRespository {
-	
+export default class DataResponsitory {
+	getFetchRespon(url) {
+		return new Promise((resolve, reject) => {
+			fetch(url)
+				.then(response => response.json())
+				.then(result => {
+					resolve(result)
+				})
+				.catch(error => {
+					reject(error)
+				})
+		})
+	}
 }
--- reactProject/js/pages/popularPage.js
+++ reactProject/js/pages/popularPage.js
@@ -2,23 +2,53 @@ import React from 'react'
 import {
     Text,
     View,
-    StyleSheet
+    StyleSheet,
+    TextInput
 } from 'react-native';
 import NavigatorBar from '../NavigatorBar'
+import DataRespon from '../expand/DataRespository'
+const URL = 'https://api.github.com/search/repositories?q='
+const QUREY_STR='&sort=stars'
 
 export default class PopularPage extends React.Component {
 	constructor(props) {
-		super(props)
+		super(props);
+		this.DataRespon = new DataRespon(),
+		this.state={
+			result: ''
+		}
 	}
-
+	onLoad() {
+		let url = this.getUrl(this.text)
+		console.log(url)
+		this.DataRespon.getFetchRespon(url)
+			.then(result=>{
+				this.setState({
+					result: JSON.stringify(result)
+				})
+			})
+			.catch(error =>{
+				this.setState({
+					result: JSON.stringify(error)
+				})
+			})
+	}
+	getUrl(key) {
+		return URL+key+QUREY_STR;
+	}
 	render() {
 		return(
 			<View style={styles.container}>
 				<NavigatorBar
 					title='最热'
-					style={{backgroundColor: '#c33'}}
+					statusBar={{backgroundColor:'red'}}
 				/>
-				<Text style={styles.tips}>获取数据</Text>
+				<Text style={styles.tips} onPress={() => {this.onLoad()}}>获取数据</Text>
+				<TextInput
+					style={{height:20,borderWidth: 1}}
+					onChangeText = {text => this.text = text}
+				/>
+				<Text style={{height:500,borderWidth: 1,marginTop: 5}}>{this.state.result}</Text>
 			</View>
 		)
 	}
@@ -26,7 +56,8 @@ export default class PopularPage extends React.Component {
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1
+		flex: 1,
+		backgroundColor: '#fff'
 	},
 	tips: {
 		fontSize: 20
